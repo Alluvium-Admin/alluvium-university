4f9f233c6e0c496e53c0b989ca78af0bbae02410
AYORINDE TIMILEHIN <38140090+timilxr@users.noreply.github.com>
2022-03-31 15:41:24 +0100

    added header and quick links

---
+++ components/nav.tsx
@@ -0,0 +1,94 @@
+import {
+  Container,
+  Navbar,
+  Nav,
+  NavDropdown,
+  Button,
+  Dropdown,
+  NavLink,
+  NavItem,
+} from "react-bootstrap";
+import type { NextComponentType } from "next";
+import Image from "next/image";
+import styles from "../styles/nav.module.scss";
+
+const Navigation: NextComponentType = () => {
+  return (
+    <>
+      <Container fluid className={styles.coverTop}>
+        <ul className="list-group list-group-horizontal px-0">
+          <li className="list-group-item border-0">Students</li>
+          <li className="list-group-item border-0">Alumni</li>
+          <li className="list-group-item border-0">Faculty & Staff</li>
+          <li className="list-group-item border-0 pe-0">
+            <input type="search" placeholder="Search" />
+          </li>
+        </ul>
+      </Container>
+      <Navbar className="w-100" collapseOnSelect expand="lg" variant="light">
+        <Container className="">
+          <Navbar.Brand href="#home">
+            <Image
+              src="/images/AULogoWhite.svg"
+              height={70}
+              width={70}
+              alt="Alluvium University Logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link className={styles.navData} href="#features">
+                Home
+              </Nav.Link>
+              <Nav.Link className={styles.navData} href="#pricing">
+                Who we are
+              </Nav.Link>
+              {/* <NavDropdown className={styles.navData} title="Courses" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown> */}
+              <Dropdown as={NavItem}>
+                <Dropdown.Toggle className={styles.navData} as={NavLink}>
+                  Courses
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item>Hello there!</Dropdown.Item>
+                  <NavDropdown.Divider />
+                  <Dropdown.Item>Hello there!</Dropdown.Item>
+                  <NavDropdown.Divider />
+                  <Dropdown.Item>Hello there!</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              <Nav.Link className={styles.navData} href="#features">
+                Campus
+              </Nav.Link>
+              <Nav.Link className={styles.navData} href="#pricing">
+                Events
+              </Nav.Link>
+              <Nav.Link className={styles.navData} href="#pricing">
+                Contact Us
+              </Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link eventKey={2} href="#memes">
+                <Button className={styles.button}>Apply</Button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Navigation;
