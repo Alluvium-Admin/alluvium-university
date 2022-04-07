553c7c8d3adc31bc553c735052543e8642bf99b5
celiaxz <93530463+Celiaxz@users.noreply.github.com>
2022-04-07 13:28:44 +0200

    removed Modal

--- components/courses/elective-courses.tsx
+++ components/courses/elective-courses.tsx
@@ -1,9 +1,9 @@
 import { NextComponentType } from "next";
 import styles from "../../styles/elective-courses.module.scss";
 import Image from "next/image";
-import { getCurriculumModalProps, ICurriculumModal } from "./curriculum";
-import { CurriculumModal } from "../modal";
-import { useState } from "react";
+// import { getCurriculumModalProps, ICurriculumModal } from "./curriculum";
+// import { CurriculumModal } from "../modal";
+// import { useState } from "react";
 
 const imageProps = {
   height: 92,
@@ -12,14 +12,14 @@ const imageProps = {
 };
 
 export const ElectiveCourses: NextComponentType = () => {
-  const [modalProps, setModalProps] = useState<ICurriculumModal>({} as any);
-  const [showModal, setShowModal] = useState<boolean>(false);
+  // const [modalProps, setModalProps] = useState<ICurriculumModal>({} as any);
+  // const [showModal, setShowModal] = useState<boolean>(false);
 
-  const onCurriculumClicked = (title: string) => {
-    const props = getCurriculumModalProps(title);
-    setModalProps(props);
-    setShowModal(true);
-  };
+  // const onCurriculumClicked = (title: string) => {
+  //   const props = getCurriculumModalProps(title);
+  //   setModalProps(props);
+  //   setShowModal(true);
+  // };
   return (
     <>
       <div className={styles.electives}>
@@ -28,30 +28,30 @@ export const ElectiveCourses: NextComponentType = () => {
           <div className={styles.courses}>
             <Image src="/images/data-center.svg" {...imageProps} />
             <span className={styles.courseTitle}>Data Center</span>
-            <span
+            {/* <span
               onClick={() => onCurriculumClicked("Data Center")}
               className={styles.curriculum}
             >
               View Curriculum
-            </span>
+            </span> */}
           </div>
           <div className={styles.courses}>
             <Image src="/images/cloud.svg" {...imageProps} />
             <span className={styles.courseTitle}>Cloud</span>
-            <span
+            {/* <span
               onClick={() => onCurriculumClicked("Cloud")}
               className={styles.curriculum}
             >
               View Curriculum
-            </span>
+            </span> */}
           </div>
         </div>
       </div>
-      <CurriculumModal
+      {/* <CurriculumModal
         {...modalProps}
         showModal={showModal}
         setShowModal={setShowModal}
-      />
+      /> */}
     </>
   );
 };
--- components/courses/mandatory-courses.tsx
+++ components/courses/mandatory-courses.tsx
@@ -1,9 +1,9 @@
 import { NextComponentType } from "next";
 import styles from "../../styles/mandatory-courses.module.scss";
 import Image from "next/image";
-import { getCurriculumModalProps, ICurriculumModal } from "./curriculum";
-import { CurriculumModal } from "../modal";
-import { useState } from "react";
+// import { getCurriculumModalProps, ICurriculumModal } from "./curriculum";
+// import { CurriculumModal } from "../modal";
+// import { useState } from "react";
 
 const imageProps = {
   height: 92,
@@ -11,14 +11,14 @@ const imageProps = {
   alt: "alluvium",
 };
 export const MandatoryCourses: NextComponentType = () => {
-  const [modalProps, setModalProps] = useState<ICurriculumModal>({} as any);
-  const [showModal, setShowModal] = useState<boolean>(false);
+  // const [modalProps, setModalProps] = useState<ICurriculumModal>({} as any);
+  // const [showModal, setShowModal] = useState<boolean>(false);
 
-  const onCurriculumClicked = (title: string) => {
-    const props = getCurriculumModalProps(title);
-    setModalProps(props);
-    setShowModal(true);
-  };
+  // const onCurriculumClicked = (title: string) => {
+  //   const props = getCurriculumModalProps(title);
+  //   setModalProps(props);
+  //   setShowModal(true);
+  // };
   return (
     <>
       <div className={styles.mandatoryCourses}>
@@ -27,30 +27,30 @@ export const MandatoryCourses: NextComponentType = () => {
           <div className={styles.courses}>
             <Image src="/images/core-business.svg" {...imageProps} />
             <span className={styles.courseTitle}>Core Business</span>
-            <span
+            {/* <span
               onClick={() => onCurriculumClicked("Core Business")}
               className={styles.curriculum}
             >
               View Curriculum
-            </span>
+            </span> */}
           </div>
           <div className={styles.courses}>
             <Image src="/images/core-data.svg" {...imageProps} />
             <span className={styles.courseTitle}>Core Data</span>
-            <span
+            {/* <span
               onClick={() => onCurriculumClicked("Core Data")}
               className={styles.curriculum}
             >
               View Curriculum
-            </span>
+            </span> */}
           </div>
         </div>
       </div>
-      <CurriculumModal
+      {/* <CurriculumModal
         {...modalProps}
         showModal={showModal}
         setShowModal={setShowModal}
-      />
+      /> */}
     </>
   );
 };
