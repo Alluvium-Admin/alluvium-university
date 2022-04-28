1618315ac4476e584a0ee9a24bad61a14c1de554
mrprotocoll <lekanvgbg@gmail.com>
2022-04-28 11:16:13 +0100

    event and contact

---
+++ components/Socialmedia.tsx
@@ -0,0 +1,15 @@
+const Socialmedia = () => {
+    return (
+        <div className="">
+            <h5>Follow Us:</h5>
+            <ul className="contact-social" style={{display: 'flex'}}>
+                <li className=""><a href="https://twitter.com/alluviumhq?s=08" rel="noreferrer" target="_blank"><img src="images/instagram.svg" alt="twitter"/></a></li>
+                <li className=""><a href="https://twitter.com/alluviumhq?s=08" rel="noreferrer" target="_blank"><img src="images/twitter.svg" alt="twitter"/></a></li>
+                <li className=""><a href="https://www.facebook.com/alluviumhq/?ti=as" rel="noreferrer" target="_blank"><img src="images/facebook.svg" alt="facebook"/></a></li>
+                <li className=""><a href="https://www.linkedin.com/company/alluvium-hq/" rel="noreferrer" target="_blank"><img src="images/linkedln.svg" alt="linkedin"/></a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Socialmedia;
--- components/contact/Content.tsx
+++ components/contact/Content.tsx
@@ -1,4 +1,5 @@
 import PropTypes  from "prop-types";
+import Socialmedia from "../Socialmedia";
 const Content = ({name} : {name:number}) => {
     switch (name){
         case 1:
@@ -13,13 +14,7 @@ const Content = ({name} : {name:number}) => {
                         </ul>
                     </div>
                     
-                    <h5>Follow Us:</h5>
-                    <ul className="contact-social" style={{display: 'flex'}}>
-                        <li className=""><a href="https://twitter.com/alluviumhq?s=08" rel="noreferrer" target="_blank"><img src="images/instagram.svg" alt="twitter"/></a></li>
-                        <li className=""><a href="https://twitter.com/alluviumhq?s=08" rel="noreferrer" target="_blank"><img src="images/twitter.svg" alt="twitter"/></a></li>
-                        <li className=""><a href="https://www.facebook.com/alluviumhq/?ti=as" rel="noreferrer" target="_blank"><img src="images/facebook.svg" alt="facebook"/></a></li>
-                        <li className=""><a href="https://www.linkedin.com/company/alluvium-hq/" rel="noreferrer" target="_blank"><img src="images/linkedln.svg" alt="linkedin"/></a></li>
-                    </ul>
+                    <Socialmedia />
                 </div>
             )
         break;
