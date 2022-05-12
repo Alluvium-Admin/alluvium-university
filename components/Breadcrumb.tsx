a6b5c8f8deb56d96eb33c66adce7d6aaa50dae33
AYORINDE TIMILEHIN <timilxr@gmail.com>
2022-05-12 16:16:25 +0100

    fix error

--- components/Breadcrumb.tsx
+++ components/Breadcrumb.tsx
@@ -1,11 +1,12 @@
 import PropTypes  from "prop-types";
+import Link from "next/link";
 
 const Breadcrumb = ({title}:{title : string}) => {
     return (
         
         <div className="breadcrumb">
             <div className="container py-3 py-md-4">
-                <p className="m-0" style={{color: '#132230'}}><a href="/" style={{color: '#009BC0', textDecoration: 'none'}}>Home </a> / {title}</p>
+                <p className="m-0" style={{color: '#132230'}}><Link href="/"><a style={{color: '#009BC0', textDecoration: 'none'}}>Home </a></Link> / {title}</p>
             </div>
         </div>
     )
